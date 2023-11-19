7bf0dbd5408cdf6ea39c63988ea9ac6d95e04124
Yura Marchenko <yurchi.marchenko@gmail.com>
2023-11-19 14:00:56 +0200

    camera follow player and add joystick

---
+++ MagisterGame/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    private Vector3 playerVector;
+    public int speed;
+
+    void Update()
+    {
+        playerVector = player.position;
+        playerVector.z = -10;
+        transform.position = Vector3.Lerp(transform.position, playerVector, speed * Time.deltaTime);
+    }
+}
--- MagisterGame/Assets/Scripts/Player.cs
+++ MagisterGame/Assets/Scripts/Player.cs
@@ -3,34 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using System.Runtime.CompilerServices;
 
 public class Player : MonoBehaviour
 {
+    private float X, Y;
     public float speed;
-
+    
     Animator anim;
     PhotonView view;
+
+    public Joystick joystick;
+    private Rigidbody2D rb;
 
     void Start()
     {
         view = GetComponent<PhotonView>();
+        rb = GetComponent<Rigidbody2D>();
+        joystick = GameObject.FindGameObjectWithTag("Joystick").GetComponent<Joystick>();
+
+        if (view.Owner.IsLocal)
+        {
+            Camera.main.GetComponent<CameraFollow>().player = gameObject.transform;
+        }
     }
 
     void Update()
     {
+        X = joystick.Horizontal * speed;
+        Y = joystick.Vertical * speed;
+
         if (view.IsMine)
         {
+            //for PC
             Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
             transform.position += input.normalized * speed * Time.deltaTime;
+            //for mobile device
+            rb.velocity = new Vector2(X, Y);
 
-            if (input == Vector3.zero)
+            /*if (input == Vector3.zero)
             {
                 anim.SetBool("isRunnig", false);
             }
             else
             {
                 anim.SetBool("isRunnig", true);
-            }
+            }*/
         }
     }
 }
