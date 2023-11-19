44f5f9c2b88efe789a5983924581eefea32cce50
Yura Marchenko <yurchi.marchenko@gmail.com>
2023-11-20 01:42:42 +0200

    sync rotate players gun on server

--- MagisterGame/Assets/Scripts/Gun.cs
+++ MagisterGame/Assets/Scripts/Gun.cs
@@ -1,24 +1,51 @@
+using Photon.Pun;
 using Photon.Pun.Demo.Asteroids;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
+public class Gun : MonoBehaviourPun
 {
     public float offset;
 
-    public GameObject bullet;
+    public GameObject bulletPrefab;
     public Transform shotPoint;
-    // Update is called once per frame
+
+    void Start()
+    {
+        // Приховати та заблокувати курсор
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
     void Update()
+    {
+        if (photonView.IsMine)
+        {
+            HandleShooting();
+        }
+    }
+
+    void HandleShooting()
     {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(bullet, shotPoint.position, transform.rotation);
+            photonView.RPC("ShootBullet", RpcTarget.AllViaServer, shotPoint.position, transform.rotation);
+        }
+    }
+
+    [PunRPC]
+    void ShootBullet(Vector3 position, Quaternion rotation)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, position, rotation);
+        PhotonView bulletPhotonView = bullet.GetComponent<PhotonView>();
+
+        if (bulletPhotonView != null)
+        {
+            bulletPhotonView.TransferOwnership(photonView.ViewID);
         }
     }
 }
