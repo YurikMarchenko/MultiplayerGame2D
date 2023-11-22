8ea8ab5f6edb7a3b1ac71e0d35f21e12089ec899
Yura Marchenko <yurchi.marchenko@gmail.com>
2023-11-23 01:17:11 +0200

    aim for shotting on win

--- MagisterGame/Assets/Scripts/Gun.cs
+++ MagisterGame/Assets/Scripts/Gun.cs
@@ -1,86 +1,90 @@
 using Photon.Pun;
-using Photon.Pun.Demo.Asteroids;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviourPun
 {
     public float offset;
+    private float X, Y;
     public float startTimeBtwShots;
     private float timeBtwShots;
+    private bool shooting = false;
 
     public GameObject bulletPrefab;
     public Transform shotPoint;
     public AudioSource audioSource;
+    public Button shootButton;
+    public Joystick joystick;
+
+    private Vector3 lastAimDirection;
+    private void Start()
+    {
+        shootButton = GameObject.FindGameObjectWithTag("ShootButton").GetComponent<Button>();
+        shootButton.onClick.AddListener(isShooting);
+        joystick = GameObject.FindGameObjectWithTag("Joystick").GetComponent<Joystick>();
+    }
+
+    void isShooting()
+    {
+        shooting = true;
+    }
 
     void Update()
     {
+        X = joystick.Horizontal * Time.deltaTime;
+        Y = joystick.Vertical * Time.deltaTime;
+
         if (photonView.IsMine)
         {
             HandleShooting();
 
-            // Зчитування положення миші для завжди вказувати у напрямку миші
+            // Зчитування положення миші для Windows
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             float rotZ = Mathf.Atan2(mousePosition.y - transform.position.y, mousePosition.x - transform.position.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+            lastAimDirection = transform.right;
         }
     }
 
-    void HandleShooting()
+    public void HandleShooting()
     {
-        if (timeBtwShots <= 0)
-        {
-            if (Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
-            {
-                Vector3 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : (Vector3)Input.mousePosition;
-
-                // Отримання локальних координат точки відносно Gun
-                Vector3 localShotPoint = shotPoint.localPosition;
-
-                // Збереження поточного обертання GUN
-                Quaternion originalRotation = transform.rotation;
-
-                // Отримання напрямку від миші або тачскріну
-                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(inputPosition);
-                Vector3 shootDirection = mousePosition - transform.position;
-
-                // Обертання GUN в напрямку стрільби
-                float rotZ = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-
-                // Створення кулі
-                photonView.RPC("ShootBullet", RpcTarget.AllViaServer, transform.TransformPoint(localShotPoint), Quaternion.LookRotation(Vector3.forward, shootDirection.normalized));
-
-                // Повернення GUN в початкове положення
-                transform.rotation = originalRotation;
-            }
-        }
-        else
+        if (timeBtwShots > 0)
         {
             timeBtwShots -= Time.deltaTime;
-            timeBtwShots = startTimeBtwShots;
         }
-    }
+
+        Vector3 shootDirection = Vector3.zero;
 
 
-    Vector3 GetShootDirection()
-    {
-        // Визначення напрямку стрільби для миші або тачскріну
-        Vector3 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : (Vector3)Input.mousePosition;
-
-        // Якщо тап справа від центру, обчислюємо напрямок стрільби
-        Vector3 difference = inputPosition - new Vector3(Screen.width / 2, Screen.height / 2);
-
-        if (difference.x <= 0)
+        if ((Input.GetKeyDown(KeyCode.Space) || shooting) && timeBtwShots <= 0 && PhotonNetwork.InRoom)
         {
-            // Якщо тап зліва від центру, повертаємо Vector3.zero, щоб не стріляти
-            return Vector3.zero;
-        }
+            Vector3 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : (Vector3)Input.mousePosition;
 
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        return Quaternion.Euler(0f, 0f, rotZ + offset) * Vector3.right;
+            // Отримання локальних координат точки відносно Gun
+            Vector3 localShotPoint = shotPoint.localPosition;
+
+            // Збереження поточного обертання GUN
+            Quaternion originalRotation = transform.rotation;
+
+            // Отримання напрямку від миші
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(inputPosition);
+            shootDirection = mousePosition - transform.position;
+
+            // Обертання GUN в напрямку стрільби
+            float rotZ = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+            // Створення кулі
+            photonView.RPC("ShootBullet", RpcTarget.AllViaServer, transform.TransformPoint(localShotPoint), Quaternion.LookRotation(Vector3.forward, shootDirection.normalized));
+
+            // Встановлюємо затримку перед наступним пострілом
+            timeBtwShots = startTimeBtwShots;
+            // Скидаємо флаг shooting
+            if (shooting)
+            {
+                shooting = false;
+            }
+        }
     }
 
     [PunRPC]
