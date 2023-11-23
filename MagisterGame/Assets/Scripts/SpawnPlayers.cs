36d32484db84f901c6d20da95ef9cbfeddca59a5
Yura Marchenko <yurchi.marchenko@gmail.com>
2023-11-23 15:00:03 +0200

    add heals spawn + area for random spawm for heals, players (all sync on server)

--- MagisterGame/Assets/Scripts/SpawnPlayers.cs
+++ MagisterGame/Assets/Scripts/SpawnPlayers.cs
@@ -7,14 +7,17 @@ public class SpawnPlayers : MonoBehaviour
 {
     public GameObject playerPrefab;
 
-    public float minX;
-    public float maxX;
-    public float minY;
-    public float maxY;
+    [SerializeField] private Rect spawnArea = new Rect(-5f, -5f, 10f, 10f);
 
+    private void OnDrawGizmos()
+    {
+        // Рисуем границы области спавна в сцене для визуализации
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(spawnArea.center, spawnArea.size);
+    }
     private void Start()
     {
-        Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        Vector2 randomPosition = new Vector2(Random.Range(spawnArea.xMin, spawnArea.xMax), Random.Range(spawnArea.yMin, spawnArea.yMax));
         PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
     }
 
